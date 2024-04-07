3038396b607cb858c0ee95e669717099f59b1b8c
Jishnu-k-b <jishnu.kb17291@gmail.com>
2024-04-07 22:01:18 +0530

    # create models user and referral

--- user/models.py
+++ user/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    referral_code = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    my_referral_code = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.username
+
+
+class Referral(models.Model):
+    referring_user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="referrals"
+    )
+    referred_user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="referred_by"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
